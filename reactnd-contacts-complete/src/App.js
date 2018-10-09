98ad660710dc3c9374e69a581b38fac531b8aeb0
fagner tonon <fagnertonon@hotmail.com>
2018-10-08 23:18:10 -0300

    set state

--- reactnd-contacts-complete/src/App.js
+++ reactnd-contacts-complete/src/App.js
@@ -27,10 +27,43 @@ const contacts = [
 
 class App extends Component {
 
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+
+    }))
+    this.setState({
+      contacts: this.state.contacts.filter((c) => c.id !== contact.id)
+    })
+  }
+
   render() {
+
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts onDeleteContact={(this.removeContact)} contacts={this.state.contacts} />
       </div>
     )
   }
